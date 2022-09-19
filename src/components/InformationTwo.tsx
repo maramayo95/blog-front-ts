10c04faeb51b11b281091431ba7fce12250c78f7
Matias Aramayo <matiasaramayo@hotmail.com>
2022-09-19 17:15:22 -0300

    Home terminado

---
+++ src/components/InformationTwo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InformationTwo = () => {
+  return (
+    <section className="py-6 dark:bg-violet-400 dark:text-gray-900">
+	<div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
+		<h1 className="text-5xl font-bold leading-none text-center">Something totally different</h1>
+		<p className="pt-2 pb-8 text-xl font-medium text-center">Eum omnis itaque harum at quae sequi unde similique alias asperiores totam. Ex cumque maxime harum doloremque, tempore nam fugiat aspernatur rerum ipsa unde est modi commodi molestias maiores eveniet eius esse asperiores neque dicta ea quisquam? Excepturi sapiente officiis explicabo ab?</p>
+		<button className="px-8 py-3 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Learn more</button>
+	</div>
+    </section>
+  )
+}
+
+export default InformationTwo
--- src/components/container/Home/Home.tsx
+++ src/components/container/Home/Home.tsx
@@ -1,18 +1,20 @@
-import Banner from './Banner'
-import Contact from './Contact'
-import Feature from './Feature'
-import Footer from './Footer'
-import Hero from './Hero'
-import Information from './Information'
+import Contact from '../../Contact'
+import Feature from '../../Feature'
+import Footer from '../../Footer'
+import Hero from '../../Hero'
+import Information from '../../Information'
+import InformationTwo from '../../InformationTwo'
+import Testimonial from '../../Testimonial'
 // import Offer from './Offer'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
-        <Feature/>
         <Information/>
-        <Banner/>
+        <InformationTwo/>
+        <Feature/>
+        <Testimonial/>
         <Contact/>
         <Footer/>
         {/* <Offer/> */}
