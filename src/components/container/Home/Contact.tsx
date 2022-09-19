f8fa3230f4fc1663dd9ab378d559541c5c59974c
Matias Aramayo <matiasaramayo@hotmail.com>
2022-09-19 10:54:32 -0300

    .

--- src/components/container/Home/Contact.tsx
+++ src/components/container/Home/Contact.tsx
@@ -10,18 +10,18 @@ const Contact = () => {
 		</div>
 		<img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
 	</div>
-	<form novalidate="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+	<form  className="space-y-6 ng-untouched ng-pristine ng-valid">
 		<div>
-			<label for="name" className="text-sm">Full name</label>
+			<label  className="text-sm">Full name</label>
 			<input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-800" />
 		</div>
 		<div>
-			<label for="email" className="text-sm">Email</label>
+			<label  className="text-sm">Email</label>
 			<input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-800" />
 		</div>
 		<div>
-			<label for="message" className="text-sm">Message</label>
-			<textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-800"></textarea>
+			<label  className="text-sm">Message</label>
+			<textarea id="message"  className="w-full p-3 rounded dark:bg-gray-800"></textarea>
 		</div>
 		<button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-400 dark:text-gray-900">Send Message</button>
 	</form>
